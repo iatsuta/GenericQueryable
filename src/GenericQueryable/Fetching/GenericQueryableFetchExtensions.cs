44d5ff198270a93ae3f5a5f111d097fea85f72aa
iatsuta <98311820+iatsuta@users.noreply.github.com>
2025-10-15 12:10:41 +0200

    upd Fetching (#4)

--- src/GenericQueryable/Fetching/GenericQueryableFetchExtensions.cs
+++ src/GenericQueryable/Fetching/GenericQueryableFetchExtensions.cs
@@ -1,14 +1,17 @@
-﻿using GenericQueryable.Fetching;
+﻿using System.Linq.Expressions;
 
-using System.Linq.Expressions;
-
-namespace GenericQueryable;
+namespace GenericQueryable.Fetching;
 
 public static class GenericQueryableFetchExtensions
 {
     public static IQueryable<TSource> WithFetch<TSource>(this IQueryable<TSource> source, string fetchPath)
     {
         return source.WithFetch(new UntypedFetchRule<TSource>(fetchPath));
+    }
+
+    public static IQueryable<TSource> WithFetch<TSource>(this IQueryable<TSource> source, Func<PropertyFetchRule<TSource>, PropertyFetchRule<TSource>> fetchRule)
+    {
+        return source.WithFetch(fetchRule(new PropertyFetchRule<TSource>([])));
     }
 
     public static IQueryable<TSource> WithFetch<TSource>(this IQueryable<TSource> source, FetchRule<TSource> fetchRule)
--- src/GenericQueryable/Fetching/PropertyFetchRule.cs
+++ src/GenericQueryable/Fetching/PropertyFetchRule.cs
@@ -1,5 +1,13 @@
-﻿namespace GenericQueryable.Fetching;
+﻿using System.Linq.Expressions;
 
-public record PropertyFetchRule<TSource>(IReadOnlyList<FetchPath> Paths) : FetchRule<TSource>, IPropertyFetchRule<TSource>;
+namespace GenericQueryable.Fetching;
+
+public record PropertyFetchRule<TSource>(IReadOnlyList<FetchPath> Paths) : FetchRule<TSource>, IPropertyFetchRule<TSource>
+{
+    public PropertyFetchRule<TSource, TNextProperty> Fetch<TNextProperty>(Expression<Func<TSource, TNextProperty>> path)
+    {
+        return new PropertyFetchRule<TSource, TNextProperty>(this.Paths.Concat([new FetchPath([path])]).ToList());
+    }
+}
 
 public record PropertyFetchRule<TSource, TLastProperty>(IReadOnlyList<FetchPath> Paths) : PropertyFetchRule<TSource>(Paths), IPropertyFetchRule<TSource, TLastProperty>;
