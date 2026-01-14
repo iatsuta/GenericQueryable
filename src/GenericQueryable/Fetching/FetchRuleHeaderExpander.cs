f3d883296a38b219bace24eb00b2e6862243ceb0
iatsuta <terraghost@mail.ru>
2026-01-14 15:02:25 +0100

    fix-FetchRuleHeaderExpander

--- src/GenericQueryable/Fetching/FetchRuleHeaderExpander.cs
+++ src/GenericQueryable/Fetching/FetchRuleHeaderExpander.cs
@@ -14,9 +14,14 @@ public class FetchRuleHeaderExpander(IEnumerable<FetchRuleHeaderInfo> fetchRuleH
     public FetchRule<TSource>? TryExpand<TSource>(FetchRule<TSource> fetchRule)
     {
         return cache.GetOrAdd(typeof(TSource),
-                _ => headersDict[typeof(TSource)].Cast<FetchRuleHeaderInfo<TSource>>()
+                _ => headersDict
+                    .GetValueOrDefault(typeof(TSource))
+                    .EmptyIfNull()
+                    .Cast<FetchRuleHeaderInfo<TSource>>()
                     .ToDictionary(info => info.Header, info => info.Implementation))
+
             .Pipe(innerCache => (IReadOnlyDictionary<FetchRule<TSource>, FetchRule<TSource>>)innerCache)
+
             .Pipe(innerCache => innerCache.GetValueOrDefault(fetchRule));
     }
 }
