359dfe4ea81a06f3ba8a7ad8b522bdb29ba64307
iatsuta <terraghost@mail.ru>
2026-02-03 12:35:11 +0100

    add GenericQueryable.Abstractions

--- src/GenericQueryable/DependencyInjection/GenericQueryableSetup.cs
+++ src/GenericQueryable/DependencyInjection/GenericQueryableSetup.cs
@@ -1,29 +1,51 @@
-﻿using GenericQueryable.Fetching;
+﻿using CommonFramework.DependencyInjection;
+
+using GenericQueryable.Fetching;
 using GenericQueryable.Services;
 
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace GenericQueryable.DependencyInjection;
 
 public class GenericQueryableSetup : IGenericQueryableSetup
 {
-    private Type targetMethodExtractorType = typeof(SyncTargetMethodExtractor);
-
     private Type fetchServiceType = typeof(IgnoreFetchService);
 
-    private readonly List<Type> fetchRuleExpanderTypeList = [typeof(FetchRuleHeaderExpander)];
+    private Type targetMethodExtractorType = typeof(SyncTargetMethodExtractor);
 
-    private readonly List<FetchRuleHeaderInfo> fetchRuleHeaderInfoList = new();
+    private readonly List<Type> fetchRuleExpanderTypeList = [];
+
+    private readonly List<FetchRuleHeaderInfo> fetchRuleHeaderInfoList = [];
 
     public void Initialize(IServiceCollection services)
     {
-        services.AddSingleton<IGenericQueryableExecutor, GenericQueryableExecutor>();
-        services.AddSingleton<IMethodRedirector, MethodRedirector>();
+        if (!services.AlreadyInitialized<IFetchRuleExpander>())
+        {
+            services.TryAddSingleton<IGenericQueryableExecutor, GenericQueryableExecutor>();
+            services.TryAddSingleton<IMethodRedirector, MethodRedirector>();
 
-        services.AddSingleton(typeof(IFetchService), this.fetchServiceType);
-        services.AddSingleton(typeof(ITargetMethodExtractor), this.targetMethodExtractorType);
+            services.AddSingleton<IFetchRuleExpander, FetchRuleHeaderExpander>();
+            services.AddKeyedSingleton<IFetchRuleExpander, RootFetchRuleExpander>(RootFetchRuleExpander.Key);
+        }
 
-        services.AddKeyedSingleton<IFetchRuleExpander, RootFetchRuleExpander>(RootFetchRuleExpander.Key);
+        if (services.AlreadyInitialized<IFetchService, IgnoreFetchService>())
+        {
+            services.ReplaceSingleton(typeof(IFetchService), this.fetchServiceType);
+        }
+        else
+        {
+            services.AddSingleton(typeof(IFetchService), this.fetchServiceType);
+        }
+
+        if (services.AlreadyInitialized<ITargetMethodExtractor, SyncTargetMethodExtractor>())
+        {
+            services.ReplaceSingleton(typeof(ITargetMethodExtractor), this.targetMethodExtractorType);
+        }
+        else
+        {
+            services.AddSingleton(typeof(ITargetMethodExtractor), this.targetMethodExtractorType);
+        }
 
         foreach (var fetchRuleExpanderType in this.fetchRuleExpanderTypeList)
         {
