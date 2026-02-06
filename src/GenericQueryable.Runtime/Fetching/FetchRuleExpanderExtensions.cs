db526843d97abbcccbb34c1a707192d0dd7656db
iatsuta <terraghost@mail.ru>
2026-02-06 14:15:31 +0100

    fix

--- src/GenericQueryable.Runtime/Fetching/FetchRuleExpanderExtensions.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace GenericQueryable.Fetching;
-
-public static class FetchRuleExpanderExtensions
-    public static PropertyFetchRule<TSource> Expand<TSource>(this IFetchRuleExpander fetchRuleExpander, FetchRule<TSource> fetchRule) =>
-        fetchRuleExpander.TryExpand(fetchRule) ?? throw new ArgumentOutOfRangeException(nameof(fetchRule));
-}
